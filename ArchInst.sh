da89546fd2dee2584b261f566181528ed85f550d
cibbia <cibbiabus@googlemail.com>
2020-03-18 12:51:14 +0000

    ArchInst

---
+++ ArchInst.sh
@@ -0,0 +1,28 @@
+sudo pacman -Su
+sudo pacman -S code
+sudo pacman -S yay
+yay -S nitrogen
+yay -S compton
+yay -S tor-browser
+yay -S timeshift
+yay -S brave
+yay -S etcher-bin
+yay -S firefox-nightly
+yay -S openrazer-meta-git
+yay -S polychromatic
+yay -S gimp
+yay -S guake
+yay -S alacritty
+yay -S rstudio-desktop-bin  
+yay -S sublime-text-dev
+yay -S xboxdrv
+yay -S steam
+yay -S gamehub-git
+yay -S vlc-git
+yay -S j4-dmenu-desktop
+yay -S lutris-git
+yay -S xclip
+yay -S xcompmgr
+yay -S feh
+yay -S broot
+yay -S wine-stable
