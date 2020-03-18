b1eaddad896f84fbb7f0052f4201050d682a117f
cibbia <cibbiabus@googlemail.com>
2020-03-18 18:16:59 +0000

    Update ArchInst.sh

--- ArchInst.sh
+++ ArchInst.sh
@@ -1,6 +1,4 @@
 sudo pacman -Syu
 sudo pacman -S yay
---noremovemake --answerclean y --answerdiff n --answeredit y --answerupgrade y --batchinstall
-code
-yay -S --noremovemake --answerclean y --answerdiff n --answeredit y --answerupgrade y --batchinstall nitrogen compton tor-browser timeshift brave etcher-bin firefox-nightly openrazer-meta-git polychromatic gimp guake alacritty rstudio-desktop-bin sublime-text-dev xboxdrv steam gamehub-git vlc-git j4-dmenu-desktop lutris-git xclip xcompmgr feh broot wine-stable
+yay -S --noremovemake --answerclean y --answerdiff n --answeredit y --answerupgrade y --batchinstall nitrogen compton tor-browser timeshift brave etcher-bin firefox-nightly openrazer-meta-git polychromatic gimp guake alacritty rstudio-desktop-bin sublime-text-dev xboxdrv steam gamehub-git vlc-git j4-dmenu-desktop lutris-git xclip xcompmgr feh broot wine-stable code
 yay -Syu
