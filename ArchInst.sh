dc3ecf3cc464c67d16fa1fde8136415cfcc4a880
cibbia <cibbiabus@googlemail.com>
2020-03-18 20:17:01 +0000

    Update ArchInst.sh

--- ArchInst.sh
+++ ArchInst.sh
@@ -1,4 +1,9 @@
 sudo pacman -Syu
 sudo pacman -S yay
-yay -S --noremovemake --answerclean y --answerdiff n --answeredit y --answerupgrade y --batchinstall nitrogen compton tor-browser timeshift brave etcher-bin firefox-nightly openrazer-meta-git polychromatic gimp guake alacritty rstudio-desktop-bin sublime-text-dev xboxdrv steam gamehub-git vlc-git j4-dmenu-desktop lutris-git xclip xcompmgr feh broot wine-stable code
+yay -S --noremovemake --answerclean y --answerdiff n --answeredit y --answerupgrade y --batchinstall git nitrogen compton tor-browser timeshift brave etcher-bin firefox-nightly openrazer-meta-git polychromatic gimp guake alacritty rstudio-desktop-bin sublime-text-dev xboxdrv steam gamehub-git vlc-git j4-dmenu-desktop lutris-git xclip xcompmgr feh broot wine-stable code
 yay -Syu
+cd ..
+cd cibbia
+git clone https://github.com/cibbia/zarch.git
+cd zarch
+sudo bash zarch.sh
